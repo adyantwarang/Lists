02ea6abf95f6ca5f61464624d6af720ab7f19b53
adyantwarang <waranganmol@gmail.com>
2026-05-27 20:36:23 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,12 @@
+emptyL = []
+print()
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+triple = [1, 2, 3] * 3
+print(triple)
+
+atlist = [100, 200, 300, 400, 500]
+atlist = atlist[::-1]
+print(atlist,"\n")
