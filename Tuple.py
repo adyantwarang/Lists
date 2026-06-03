5ee651edf9d83cf6c5f44db7af59483262e4b893
adyantwarang <waranganmol@gmail.com>
2026-06-03 20:36:35 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,24 @@
+#creating a tuple and giving it data
+tuplex = ("Rhino", True, 6.9, 4)
+print(tuplex)
+
+#creating the tuple
+tuplex = (7, 9, 4, 8, 5, 3)
+print(tuplex)
+
+#adding 9 to the tuple
+tuplex = tuplex + (9,)
+print(tuplex)
+
+#creating a brand new tuple
+tuple1 = (90, 80, 20, 40, 70)
+print(tuple1.count(70))
+
+#adding data to the orignal tuple
+tuplex = (7, 9, 4, 8, 5, 3, 1, 2, 6)
+slice = tuplex[3:7]
+print(slice)
+
+#data
+slice = tuplex[:8]
+print(slice)
